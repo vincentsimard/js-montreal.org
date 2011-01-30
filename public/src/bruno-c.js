3d196baabccbf847b351b354e74f768de0605b7f
bruno-c <neonskimmer@gmail.com>
2011-01-29 23:04:22 -0500

    lalala lalala

--- public/src/bruno-c.js
+++ public/src/bruno-c.js
@@ -1,28 +1,36 @@
 // bruno-c
-// my js-montreal member js signature thingy
 // neonskimmer@gmail.com
+// js-montreal.org plugin - javascript links
+// It gets a bunch of js related links from reddit, ajaxian, etc
+// using a yahoo pipe and displays them leisurely, without a care in the world.
+// One page resume / crappy site at http://brunocarriere.com
+ 
 $(function(){
 
-  var colors = ['blue', 'green', 'yellow', 'purple']
-    , $ol = $('<ol/>', { id: 'fun', class: 'fun' }).insertAfter("#bd")
-    , $lies_damn_lies = colors.map( function(c){ return $('<li/>', { class: c }).appendTo($ol); });
-    
-  function jslink(title, href){
-    return $( '<a class="jslink" target="_new" title="'
-            + title + '" href="' + href + '">'
-            + title + '</a>');
+  var colors  = ['blue', 'green', 'yellow', 'purple']
+    , ol      = $('<ol/>', { id: 'fun', class: 'fun' }).insertAfter("#bd")
+    // Lies damn lies, you can't handle the truth, etc.
+    , lies    = colors.map( function(c){ return $('<li/>', { class: c }).appendTo(ol); });
+
+// Returns a shiny new link.
+  function jslink( title, href ){
+    return $( '<a/>', { class   : "jslink"
+                      , target  : "_new"
+                      , title   : title
+                      , href    : href
+                      , text    : title });
   }
   
-  function refresh(links){
+  function refresh( links ){
     
-    for (var i=0; i<4; i++){1
-      
+    for ( var i = 0; i < 4; i++ ){
+
       var link = links.next()
         , a    = jslink(link.title, link.link).hide();
-        
+
 // Replace the HTML of each link list item
 
-      $( $lies_damn_lies[i] ).html( a );
+      $( lies[i] ).html( a );
       
 // Fade in the links one a time with some delay between them
 
@@ -36,27 +44,28 @@ $(function(){
   
   
 // Fetch data from the yahoo pipe
+// The pipe contains good shit.
 
-  $.getJSON('data/js-links', function(feed){		
+  $.getJSON( 'data/js-links', function(feed){		
     
-    if (feed.length === 0){
-      return;
-    }
+    if (feed.length === 0) return;
     
     var jslinks = (function(){
-    
+          
       var arr = feed
         , idx = -1;
-      
-      arr.next = function(){
-        return arr[ ++idx % this.length ];
+
+// Clever girl.
+
+      arr.next = function(){ 
+        return this[ ++idx % this.length ];
       };
       
-      return arr;			
+      return arr;
+      
+    }());
     
-    })();
-    
-    refresh(jslinks);
+    refresh( jslinks );
   });
   
 });
